2a7f461c397c89896a9637b49fd480400e7be3ad
Fazeel Ahmed <faxeelgr8@gmail.com>
2019-04-03 11:24:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var name=prompt("Enter Your Name");
 
-if(name!= null){
+
+var nullValue = "Your Name";
+var name = prompt("PLease Enter Your Full Name: ",nullValue);
+
+if(name === null){
+	document.getElementById("nameTxt").innerText = "Your Name";
+}else if (name){
 	document.getElementById("nameTxt").innerText = name;
+}
+else{
+	document.getElementById("nameTxt").innerText = "Your Name";	
 }
